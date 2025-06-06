9ead03e25af90c5a0967d279e5096fa64687d60d
IAMHUDA <hudamiftahul362@gmail.com>
2025-06-06 13:08:41 +0700

    add buyer screen

--- lib/presentation/buyer/profile/bloc/profile_buyer_bloc.dart
+++ lib/presentation/buyer/profile/bloc/profile_buyer_bloc.dart
@@ -2,7 +2,7 @@ import 'package:bloc/bloc.dart';
 import 'package:canary_farm/data/models/request/buyer/buyer_profile_request_model.dart';
 import 'package:canary_farm/data/models/response/buyer/buyer_profile_response_model.dart';
 import 'package:canary_farm/data/repository/profile_buyer_repository.dart';
-import 'package:meta/meta.dart';
+
 
 part 'profile_buyer_event.dart';
 part 'profile_buyer_state.dart';
