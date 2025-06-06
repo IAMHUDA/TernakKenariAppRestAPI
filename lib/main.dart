75c783e7a8c637c30fe0610a17a752cf2c9aef59
IAMHUDA <hudamiftahul362@gmail.com>
2025-06-06 13:32:46 +0700

    add main.dart multiblocprovider

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,13 @@
+import 'package:canary_farm/data/repository/auth_repository.dart';
+import 'package:canary_farm/data/repository/get_all_burung_tersedia_repository.dart';
+import 'package:canary_farm/data/repository/profile_buyer_repository.dart';
+import 'package:canary_farm/presentation/auth/bloc/login/login_bloc.dart';
+import 'package:canary_farm/presentation/auth/bloc/register/register_bloc.dart';
+import 'package:canary_farm/presentation/bloc/get_all_burung_tersedia/bloc/get_burung_tersedia_bloc.dart';
+import 'package:canary_farm/presentation/buyer/profile/bloc/profile_buyer_bloc.dart';
+import 'package:canary_farm/services/service_http_client.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,59 +18,30 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) =>
+          LoginBloc(authRepository:AuthRepository(ServiceHttpClient()))
+          ),
+        BlocProvider(
+          create: (context) =>
+          RegisterBloc(authRepository:AuthRepository(ServiceHttpClient()))
+          ),
+        BlocProvider(
+          create: (context) =>
+          ProfileBuyerBloc(profileBuyerRepository: ProfileBuyerRepository(ServiceHttpClient()))
+          ),
+        BlocProvider(
+          create: (context) => GetBurungTersediaBloc(GetAllBurungTersediaRepository(ServiceHttpClient())))
+      ], 
+      child: MaterialApp(
+        title: 'flutter demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple)
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
+        home: const Splashscreen(),
+      ));
   }
 }
+
